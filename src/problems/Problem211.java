43eea54a5f3ed8a289ce66cdd8570e8e22276c76
igor <igor27school@gmail.com>
2012-12-14 19:15:41 -0800

    New solution (runs much faster, using seive)

--- src/problems/Problem211.java
+++ src/problems/Problem211.java
@@ -1,75 +1,48 @@
 package problems;
 
-import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Problem211 {
 	
-	private static Map<Long, BigInteger> numbersAndSums = 
-		new HashMap<Long, BigInteger>();
+	private static long[] sums;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
 		int MAX = 64000000;
-		long answer = 1;
-		Util.init(MAX);
-		System.out.println("Init complete");
+		long answer = 0;
 		populateMap(MAX);
 		System.out.println("Populate complete");
-		for (int i=2; i<MAX; i++) {
-			// Primes won't produce square sum: p^2 + 1
-			if (Util.is_prime(i))
-				continue;
-			List<Integer> primes = Util.getPrimeFactorization(i);
-			if (is_prefect_square(primes)) {
+		for (int i=1; i<MAX; i++) {
+			if (is_prefect_square(sums[i])) {
 				System.out.println("The number is " + i);
 				answer += i;
 			}
 		}
 		System.out.println("The answer is " + answer);
+		long end = System.currentTimeMillis();
+		System.out.println("The program took " + (end - start)/1000 + 
+				" seconds to complete");
 	}
 
-	// f(p^m * q^n) = f(p^m) * f(q^n)
-	private static boolean is_prefect_square(List<Integer> primes) {
-		BigInteger counter = BigInteger.ONE;
-		for (Integer prime : primes) {
-			counter = counter.multiply(numbersAndSums.get((long)prime));
-		}
-		return is_perfect_square(counter);
+	private static boolean is_prefect_square(long sum) {
+		long sqrt = Util.get_sqrt(sum);
+		return sqrt * sqrt == sum;
 	}
 
 	private static void populateMap(int max) {
-		int number;
-		long counter;
+		sums = new long[max];
+		// Start with 1 for every value of array
+		Arrays.fill(sums, 1);
+		long square;
 		// Populate numbersAndSums map
 		for (int i=2; i<max; i++) {
-			if (!Util.is_prime(i))
-				continue;
-			number = i;
-			counter = number;
-			while (counter < max) {
-				BigInteger sum = get_sum(counter, number);
-				numbersAndSums.put(counter, sum);
-				counter *= number;
+			square = (long)i * i;
+			for (int j=i; j<max; j+=i) {
+				sums[j] += square;
 			}
 		}
-		
 	}
-
-	private static boolean is_perfect_square(BigInteger sum) {
-		long sqrt = Util.get_sqrt(sum.longValue());
-		return sqrt * sqrt == sum.longValue();
-	}
-
-	// f(p^m) = 1 + p^2 + ... + p^(2m) = (1 - p^(2m+2)) / (1 - p^2)
-	private static BigInteger get_sum(long counter, int prime) {
-		BigInteger b = BigInteger.valueOf((long)counter * prime);
-		return b.pow(2).subtract(BigInteger.ONE).
-		divide(BigInteger.valueOf(prime).pow(2).subtract(BigInteger.ONE));
-	}
-
 }
